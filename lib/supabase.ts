e513dee30cd5b9f3447dcef7b83aab56e39642c4
Sreechand Tavva <sreechandt@gmail.com>
2025-05-23 07:58:53 +0530

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.https://fednnljfnffdtdxscbhr.supabase.co;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlZG5ubGpmbmZmZHRkeHNjYmhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxMzA1NjUsImV4cCI6MjA1OTcwNjU2NX0.QBR22naUyHM9kT2sdcQBQAjbIH4wILniScbjXq82H2M;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
